bc81f104d90f7b6d55c07f43abaf5031d3fb8a8f
Kuberan B <kuberanbaluchami@gmail.com>
2022-12-27 17:15:36 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/woodie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.woodie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Colors.black;
+
+const kWhiteColor = Colors.white;
+
+const kBlackColor = Colors.black;
+
+const kGreyColor = Colors.grey;
+
+const kListTileColor = Color.fromARGB(255, 26, 26, 26);  
+
+const kBlueColor = Colors.blue;
+
+const kspecialGrey = Color(0xFF4b4e54); 
+
+const kLightGreen = Colors.green;
+
+const kRedColor = Colors.red;
+
+const kBottomNavigationIconColor = Color.fromARGB(255, 136, 137, 137);
